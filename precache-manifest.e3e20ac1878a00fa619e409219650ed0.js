663f88ad159fb4be95652dbc545021272fb4d924
hellonanda <nandapuspitaranagmail.com>
2020-10-12 23:18:43 +0700

    first init

---
+++ precache-manifest.e3e20ac1878a00fa619e409219650ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbbbb1561178523d2ed3b192f220e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e050bfc0246054367c3f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e050bfc0246054367c3f",
+    "url": "/static/js/main.efc3dd7b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
